be77f1e38cc1babaeec1817fa262de20d36a6e8b
Nguyễn Văn Thiên <50241083+hoangthien10021998@users.noreply.github.com>
2023-05-22 16:21:06 +0700

    backup db mysql mariadb by mysql dump
    backup db mysql mariadb by mysql dump

---
+++ backupsql_2.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+# Set variables
+USER="root"
+PASSWORD="Sds@123"
+DATABASE="thiennv"
+BACKUP_DIR="/home/backup"
+MAX_AGE=7
+
+# Create backup directory if it does not exist
+if [ ! -d "$BACKUP_DIR" ]; then
+  mkdir -p "$BACKUP_DIR"
+fi
+
+# Create backup file name
+BACKUP_FILE="$BACKUP_DIR/$DATABASE-$(date +%Y-%m-%d_%H-%M-%S).sql"
+
+# Create backup using mysqldump
+mysqldump --user=$USER --password=$PASSWORD $DATABASE > $BACKUP_FILE
+
+# Remove backups older than MAX_AGE days
+find $BACKUP_DIR -type f -name "$DATABASE-*.sql" -mtime +$MAX_AGE -exec rm {} \;
